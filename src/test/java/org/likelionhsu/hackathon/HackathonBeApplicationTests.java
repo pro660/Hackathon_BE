6be1577d0c4164db29c24da8349e221bd16c611c
developer-sw <swj1234567@naver.com>
2026-07-25 21:54:20 +0900

    chore: Spring Boot 프로젝트 초기 설정

---
+++ src/test/java/org/likelionhsu/hackathon/HackathonBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.likelionhsu.hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
