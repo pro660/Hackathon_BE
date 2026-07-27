86b1ae6cef2088b50600fbd95f4658a525e67f3c
developer-sw <swj1234567@naver.com>
2026-07-27 23:05:01 +0900

    chore: Spring Boot 프로젝트 초기 설정 (#1)
    chore/project-init의 내용을 main에 합치기.

---
+++ src/main/java/org/likelionhsu/hackathon/HackathonBeApplication.java
@@ -0,0 +1,13 @@
+package org.likelionhsu.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonBeApplication.class, args);
+	}
+
+}
