e6239034c28299025942ab22baf134af16829b1c
developer-sw <swj1234567@naver.com>
2026-08-05 23:10:33 +0900

    chore: H2 기반 테스트 프로필 구성

--- src/test/java/org/likelionhsu/hackathon/HackathonBeApplicationTests.java
+++ src/test/java/org/likelionhsu/hackathon/HackathonBeApplicationTests.java
@@ -1,13 +1,34 @@
 package org.likelionhsu.hackathon;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
 @SpringBootTest
 class HackathonBeApplicationTests {
+
+	@Autowired
+	private DataSource dataSource;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void testDatabaseUsesH2() throws SQLException {
+		try (Connection connection = dataSource.getConnection()) {
+			String databaseUrl = connection.getMetaData().getURL();
+
+			assertTrue(databaseUrl.startsWith("jdbc:h2:mem:"));
+		}
+	}
 }
