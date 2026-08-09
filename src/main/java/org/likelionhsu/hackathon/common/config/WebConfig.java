1204818c9ca515875264838898f58ce4ac46dbd4
q9622 <q273493367@gmail.com>
2026-08-09 13:01:16 +0900

    chore: 환경별 설정 및 환경변수 구성

--- src/main/java/org/likelionhsu/hackathon/common/config/WebConfig.java
+++ src/main/java/org/likelionhsu/hackathon/common/config/WebConfig.java
@@ -1,5 +1,6 @@
 package org.likelionhsu.hackathon.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration(proxyBeanMethods = false)
 public class WebConfig implements WebMvcConfigurer {
 
+    private final String allowedOrigin;
+
+    public WebConfig(
+            @Value("${app.cors.allowed-origin}")
+            String allowedOrigin
+    ) {
+        this.allowedOrigin = allowedOrigin;
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(allowedOrigin)
                 .allowedMethods(
                         "GET",
                         "POST",
