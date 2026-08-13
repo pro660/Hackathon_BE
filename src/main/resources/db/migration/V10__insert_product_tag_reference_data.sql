1821cfc621d9ea7f610823c0cb5b7f0cdf29f664
developer-sw <swj1234567@naver.com>
2026-08-13 18:31:55 +0900

    [FEAT] ProductTag 기준 데이터 추가

---
+++ src/main/resources/db/migration/V10__insert_product_tag_reference_data.sql
@@ -0,0 +1,26 @@
+INSERT INTO product_tags (
+    type,
+    code
+) VALUES
+      ('STYLE', 'CASUAL'),
+      ('STYLE', 'FORMAL'),
+      ('STYLE', 'NEAT'),
+      ('STYLE', 'GLAMOROUS'),
+
+      ('SEASON', 'SPRING'),
+      ('SEASON', 'SUMMER'),
+      ('SEASON', 'AUTUMN'),
+      ('SEASON', 'WINTER'),
+      ('SEASON', 'ALL_SEASON'),
+
+      ('OCCASION', 'DAILY'),
+      ('OCCASION', 'DATE'),
+      ('OCCASION', 'TRAVEL'),
+      ('OCCASION', 'GATHERING'),
+      ('OCCASION', 'CEREMONY'),
+      ('OCCASION', 'OUTDOOR'),
+      ('OCCASION', 'OTHER'),
+
+      ('FEATURE', 'COMPACT'),
+      ('FEATURE', 'SPACIOUS'),
+      ('FEATURE', 'MULTIWAY');
